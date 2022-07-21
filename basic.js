2855e6d13892f7eba193d3481e1cbc3d095ec311
MAULIK RAJENDRA PATEL <maulikpatelmow@gmail.com>
2022-07-21 18:43:11 +0530

    variables
    with examples

--- basic.js
+++ basic.js
@@ -1,16 +1,44 @@
 console.log("run script")
-// function
-// alert();
-alert("THis is the alert to Maulik");
-
-// variables
-var firsrName = "Maulik";
-firstName;
-
-// Combining Strings
-alert(firstName + " is awesome!");
 
 // example
-alert(3 +
-    1
-    + 2);
+        var a = 33;
+        var b = 10;
+        var c = "Test";
+        var linebreak = "<br />";
+
+        document.write("a + b = ");
+        result = a + b;
+        document.write(result);
+        document.write(linebreak);
+
+        document.write("a - b = ");
+        result = a - b;
+        document.write(result);
+        document.write(linebreak);
+
+        document.write("a / b = ");
+        result = a / b;
+        document.write(result);
+        document.write(linebreak);
+
+        document.write("a % b = ");
+        result = a % b;
+        document.write(result);
+        document.write(linebreak);
+
+        document.write("a + b + c = ");
+        result = a + b + c;
+        document.write(result);
+        document.write(linebreak);
+
+        a = ++a;
+        document.write("++a = ");
+        result = ++a;
+        document.write(result);
+        document.write(linebreak);
+
+        b = --b;
+        document.write("--b = ");
+        result = --b;
+        document.write(result);
+        document.write(linebreak);
