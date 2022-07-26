a764c3f94162aa7e9fd3168891b978ea944a66de
MAULIK RAJENDRA PATEL <maulikpatelmow@gmail.com>
2022-07-26 12:52:55 +0530

    output
    4 methods

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
 console.log("Hello world by using External means")
+
+// JavaScript can "display" data in different ways:
+//  Writing into an HTML element, using innerHTML.
+//  Writing into the HTML output using document.write().
+//  Writing into an alert box, using window.alert().
+//  Writing into the browser console, using console.log().
