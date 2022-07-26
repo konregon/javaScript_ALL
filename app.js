74aa4d361ebc1f87bcab51319532298da0e3c669
MAULIK RAJENDRA PATEL <maulikpatelmow@gmail.com>
2022-07-26 15:18:56 +0530

    update

--- app.js
+++ app.js
@@ -3,5 +3,16 @@ console.log("Hello world by using External means")
 // JavaScript can "display" data in different ways:
 //  Writing into an HTML element, using innerHTML.
 //  Writing into the HTML output using document.write().
+//      Only use this for testing. Red flag
 //  Writing into an alert box, using window.alert().
 //  Writing into the browser console, using console.log().
+
+
+
+// A computer program is a list of "instructions" to be "executed" by a computer.
+// In a programming language, these programming instructions are called statements.
+// A JavaScript program is a list of programming statements.
+
+// JavaScript statements are composed of:
+// Values, Operators, Expressions, Keywords, and Comments.
+// This statement tells the browser to write "Hello Dolly." inside an HTML element with id="demo":
