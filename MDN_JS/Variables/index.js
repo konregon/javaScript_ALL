f850acd3e326d53955f2716bf4fe2daccfdfce40
MAULIK RAJENDRA PATEL <maulikpatelmow@gmail.com>
2022-08-15 17:21:45 +0530

    update

---
+++ MDN_JS/Variables/index.js
@@ -0,0 +1,26 @@
+let myName;
+let myAge;
+ 
+myName = "Maulik";
+myAge = "26";
+
+
+let dolphinGoodbye = 'So long and thanks for all the fish';
+
+let color;
+
+let myNameArray = ['Chris', 'Bob', 'Jim'];
+let myNumberArray = [10, 15, 40];
+
+myNameArray[0]; // should return 'Chris'
+myNumberArray[2]; // should return 40
+
+let dog = { name : 'black', breed : 'local'};
+
+let myName1;
+myName1 = 'Maulik';
+let age1 = 27;
+
+
+
+
